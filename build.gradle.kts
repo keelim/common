17ff3ce6a2023f2aeedca92d83a94169ded22bfe
Keel_im <kimh00335@gmail.com>
2022-06-21 22:06:57 +0900

    update common module

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,5 +19,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.material.themAdapter)
 }
 
