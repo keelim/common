6e67f3ab8282da3242c022163b38943285276ebd
keelim <kimh00335@gmail.com>
2022-06-25 15:09:31 +0900

    update the module

--- src/main/java/com/keelim/common/extensions/ContextExt.kt
+++ src/main/java/com/keelim/common/extensions/ContextExt.kt
@@ -69,14 +69,14 @@ fun Context.executeWeb(url: String?) {
 }
 
 private fun Context.startBrowserActivity(url: String) {
-    CustomTabsIntent.Builder()
-        .setShareState(CustomTabsIntent.SHARE_STATE_ON)
-        .setColorScheme(CustomTabsIntent.COLOR_SCHEME_SYSTEM)
-        .setShowTitle(true)
-        .setStartAnimations(this, R.anim.fade_in, R.anim.fade_out)
-        .setExitAnimations(this, R.anim.fade_in, R.anim.fade_out)
-        .build()
-        .launchUrl(this, Uri.parse(url))
+//    CustomTabsIntent.Builder()
+//        .setShareState(CustomTabsIntent.SHARE_STATE_ON)
+//        .setColorScheme(CustomTabsIntent.COLOR_SCHEME_SYSTEM)
+//        .setShowTitle(true)
+//        .setStartAnimations(this, R.anim.fade_in, R.anim.fade_out)
+//        .setExitAnimations(this, R.anim.fade_in, R.anim.fade_out)
+//        .build()
+//        .launchUrl(this, Uri.parse(url))
 }
 
 private fun Context.startNonBrowserActivity(url: String, fallback: () -> Unit) {
