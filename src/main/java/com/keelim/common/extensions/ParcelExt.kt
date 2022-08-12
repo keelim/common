db6d49e17cbae975aa822c2978fb62b1b9776f41
Keel_im <kimh00335@gmail.com>
2022-08-12 22:46:52 +0900

    update common module

---
+++ src/main/java/com/keelim/common/extensions/ParcelExt.kt
@@ -0,0 +1,15 @@
+package com.keelim.common.extensions
+
+import android.content.Intent
+import android.os.Build.VERSION.SDK_INT
+import android.os.Bundle
+
+private inline fun <reified T> Intent.getParcel(key: String, clazz: Class<T>): T? = when {
+    SDK_INT >= 33 -> getParcelableExtra(key, clazz)
+    else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+}
+
+private inline fun <reified T> Bundle.getParcel(key: String, clazz: Class<T>): T? = when {
+    SDK_INT >= 33 -> getParcelable(key, clazz)
+    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
